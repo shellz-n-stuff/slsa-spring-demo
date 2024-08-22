73ad33087d744cfeeca00e1954ddba4e6a1e8105
Alex Rosenzweig <64241648+shellz-n-stuff@users.noreply.github.com>
2024-08-22 19:26:22 +1000

    feat: Improving responses

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot and Jib!";
+        return "Greetings from Spring Boot and Jib and AppSec Australia!";
     }
 }
